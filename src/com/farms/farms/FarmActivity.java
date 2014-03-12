f11416140d7655a07715087f223a4255fcfb9842
Anantha Kumar <ananthakumar@valiantsystems.com>
2014-03-12 15:22:44 +0530

    march12

--- src/com/farms/farms/FarmActivity.java
+++ src/com/farms/farms/FarmActivity.java
@@ -9,7 +9,41 @@ public class FarmActivity extends DroidGap{
     @Override
     public void onCreate(Bundle savedInstanceState) {    	
     	super.onCreate(savedInstanceState);
-//    	super.setIntegerProperty("splashscreen",R.drawable.ic_launcher);
+    	super.setIntegerProperty("splashscreen",R.drawable.ic_launcher);
     	super.loadUrl("file:///android_asset/www/index.html"); 
     }    
 }
+/*public class SplashActivity extends Activity
+{
+    private static final long DELAY = 3000;
+    private boolean scheduled = false;
+    private Timer splashTimer;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        splashTimer = new Timer();
+        splashTimer.schedule(new TimerTask()
+        {
+            @Override
+            public void run()
+            {
+                SplashActivity.this.finish();
+                startActivity(new Intent(SplashActivity.this, FarmActivity.class));
+            }
+         }, DELAY);
+       scheduled = true;
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+        if (scheduled)
+            splashTimer.cancel();
+        splashTimer.purge();
+    }
+}*/
